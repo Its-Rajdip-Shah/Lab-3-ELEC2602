8adb600e44740ed6c71bde9491a0f7789730699c
Its-Rajdip-Shah <its.rajdip.shah@gmail.com>
2026-03-23 13:50:36 +1100

    final implementation

--- part2/lab3_P2_TB.v
+++ part2/lab3_P2_TB.v
@@ -2,9 +2,7 @@
  
 module lab3_P2_TB;
  
-	reg [3:0] count;
-	reg [1:0] S,U,V,W,X;
-	
+	reg [1:0] S, U, V, W, X;
 	wire [6:0] output_7seg;
 
 	lab3_P2 instantiate_p5 (
@@ -20,24 +18,29 @@ module lab3_P2_TB;
 		$dumpfile("sim/lab3_p2.vcd");
 		$dumpvars(0, lab3_P2_TB);
 
-		count = 4'b0000;
-		#300;
+		// -----------------------------
+		// Test set 1:
+		// U = 00, V = 01, W = 10, X = 11
+		// -----------------------------
+		U = 2'b00; V = 2'b01; W = 2'b10; X = 2'b11;
+
+		S = 2'b00; #20;   // m = 00, d = 0000111
+		S = 2'b01; #20;   // m = 01, d = 1110000
+		S = 2'b10; #20;   // m = 10, d = 0011100
+		S = 2'b11; #20;   // m = 11, d = 1100011
+
+		// -----------------------------
+		// Test set 2:
+		// U = 01, V = 10, W = 11, X = 00
+		// -----------------------------
+		U = 2'b01; V = 2'b10; W = 2'b11; X = 2'b00;
+
+		S = 2'b00; #20;   // m = 01, d = 1110000
+		S = 2'b01; #20;   // m = 10, d = 0011100
+		S = 2'b10; #20;   // m = 11, d = 1100011
+		S = 2'b11; #20;   // m = 00, d = 0000111
+
 		$finish;
-	end
-	
-	always begin
-		#50
-		count = count + 4'b0001;
-	end
-	
-	always @(*) begin
-		case (count)
-			4'b0000 : begin S = 2'b00; U = 2'b00; V = 2'b01; W = 2'b10; X = 2'b11; end
-			4'b0001 : begin S = 2'b01; U = 2'b00; V = 2'b01; W = 2'b10; X = 2'b11; end
-			4'b0010 : begin S = 2'b10; U = 2'b00; V = 2'b01; W = 2'b10; X = 2'b11; end
-			4'b0011 : begin S = 2'b11; U = 2'b00; V = 2'b01; W = 2'b10; X = 2'b11; end
-			default : begin S = 2'b00; U = 2'b11; V = 2'b10; W = 2'b01; X = 2'b00; end
-		endcase
 	end
 
 endmodule
